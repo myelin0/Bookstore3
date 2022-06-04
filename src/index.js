000d6aeed9d0c7a1a30fca7a780f17fb431db16d
myelin0 <emi.veisllari@gmail.com>
2022-06-04 08:28:36 +0200

    Add Eslint and stylelint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 
-
 import NavBar from './components/NavBar';
 import Books from './components/Books';
 import Categories from './components/Categories';
@@ -12,13 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Books />} />
-      <Route path="/Categories" element={<Categories />} />
-    </Routes>
-  </Router>,
-  </React.StrictMode>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </Router>
+    ,
+  </React.StrictMode>,
 );
-
-
